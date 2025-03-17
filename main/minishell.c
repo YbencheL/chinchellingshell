57da018c51e8c2bad757b9d030e2a01ce854f367
Yassine Benchelh <ybenchel@e1r11p12.1337.ma>
2025-03-17 19:15:57 +0000

    add some adjustment

--- main/minishell.c
+++ main/minishell.c
@@ -6,7 +6,7 @@
 /*   By: ybenchel <ybenchel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/15 14:13:52 by ybenchel          #+#    #+#             */
-/*   Updated: 2025/03/17 19:13:36 by ybenchel         ###   ########.fr       */
+/*   Updated: 2025/03/17 19:15:49 by ybenchel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,10 +43,10 @@ void shell_loop(t_mp *pg)
 			if (check_syntax_error(tokens))
 				allocation_fails();
 
-			// Step 3: Expand Variables ($VAR and $?)
+			// Step 3: Expand Variables (Replacing $VAR with it's value and handling $?)
 			expand_variables(tokens, pg->env);
 
-			// Step 4: Handle Quotes Double and single
+			// Step 4: Handle Quotes (Double "" and single ')
 			handle_quotes(tokens);
 
 			// Step 5: Convert Tokens to Commands
