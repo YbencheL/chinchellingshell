0e38f269dd37873a75d502c51da89b71022724f3
Yassine Benchelh <ybenchel@e1r10p11.1337.ma>
2025-03-19 17:55:26 +0000

    add some adjustment

--- tools/tool4.c
+++ tools/tool4.c
@@ -6,7 +6,7 @@
 /*   By: ybenchel <ybenchel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 17:25:20 by ybenchel          #+#    #+#             */
-/*   Updated: 2025/03/19 17:47:11 by ybenchel         ###   ########.fr       */
+/*   Updated: 2025/03/19 17:55:00 by ybenchel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,16 @@ t_token_type	token_type(char *str)
 {
 	t_token_type 	type;
 
-	if(ft_strcmp(str, "|"))
-		return type = PIPE;
-	else if(ft_strcmp(str, "<"))
-		return type = RED_IN;
-	else if(ft_strcmp(str, ">"))
-		return type = RED_OUT;
-	else if(ft_strcmp(str, ">>"))
-		return type = RED_APPEND;
-	else if(ft_strcmp(str, "<<"))
-		return type = HEREDOC;
+	if(ft_strcmp(str, "|") == 0)
+		return PIPE;
+	else if(ft_strcmp(str, "<") == 0)
+		return RED_IN;
+	else if(ft_strcmp(str, ">") == 0)
+		return RED_OUT;
+	else if(ft_strcmp(str, ">>") == 0)
+		return RED_APPEND;
+	else if(ft_strcmp(str, "<<") == 0)
+		return HEREDOC;
 	else
 		return type = WORD;
 }
