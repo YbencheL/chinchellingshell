57e592916ebd69c862e7caeaa981bc848ef4e939
Abdellah Benzahour <abenzaho@e1r11p10.1337.ma>
2025-04-08 17:50:08 +0100

    reset this shit

--- parsing/parsing3.c
+++ parsing/parsing3.c
@@ -3,63 +3,63 @@
 /*                                                        :::      ::::::::   */
 /*   parsing3.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ybenchel <ybenchel@student.42.fr>          +#+  +:+       +#+        */
+/*   By: abenzaho <abenzaho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/29 16:50:55 by ybenchel          #+#    #+#             */
-/*   Updated: 2025/04/06 16:21:08 by ybenchel         ###   ########.fr       */
+/*   Updated: 2025/04/08 17:49:02 by abenzaho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-char	*search_env(char *var, t_mp *ev)
-{
-	int		i;
-	char	*new_var;
-	char	*equal_sign;
-	int		len;
+// char	*search_env(char *var, t_mp *ev)
+// {
+// 	int		i;
+// 	char	*new_var;
+// 	char	*equal_sign;
+// 	int		len;
 
-	i = 0;
-	new_var = ft_malloc(sizeof(char) * (ft_strlen(&var[1]) + 2));
-	if (!new_var)
-		allocation_fails();
-	ft_strlcpy(new_var, &var[1], ft_strlen(&var[1]) + 1);
-	len = ft_strlen(new_var);
-	new_var[len] = '=';
-	new_var[len + 1] = '\0';
-	while (ev->envp[i])
-	{
-		if (ft_strncmp(ev->envp[i], new_var, ft_strlen(new_var)) == 0)
-		{
-			equal_sign = ft_strchr(ev->envp[i], '=');
-			if (equal_sign)
-				return (equal_sign + 1);
-		}
-		i++;
-	}
-	return (NULL);
-}
+// 	i = 0;
+// 	new_var = ft_malloc(sizeof(char) * (ft_strlen(&var[1]) + 2));
+// 	if (!new_var)
+// 		allocation_fails();
+// 	ft_strlcpy(new_var, &var[1], ft_strlen(&var[1]) + 1);
+// 	len = ft_strlen(new_var);
+// 	new_var[len] = '=';
+// 	new_var[len + 1] = '\0';
+// 	while (ev->envp[i])
+// 	{
+// 		if (ft_strncmp(ev->envp[i], new_var, ft_strlen(new_var)) == 0)
+// 		{
+// 			equal_sign = ft_strchr(ev->envp[i], '=');
+// 			if (equal_sign)
+// 				return (equal_sign + 1);
+// 		}
+// 		i++;
+// 	}
+// 	return (NULL);
+// }
 
-char	*replace_var(char *token, char *var, char *value)
-{
-	char	*new_string;
-	char	*var_pos;
-	int		before_var_len;
-	int		total_len;
+// char	*replace_var(char *token, char *var, char *value)
+// {
+// 	char	*new_string;
+// 	char	*var_pos;
+// 	int		before_var_len;
+// 	int		total_len;
 
-	var_pos = ft_strstr(token, var);
-	if (!var_pos)
-		return (ft_strdup(token));
-	before_var_len = var_pos - token;
-	total_len = ft_strlen(token) - ft_strlen(var) + ft_strlen(value);
-	new_string = ft_malloc(sizeof(char) * (total_len + 1));
-	if (!new_string)
-		allocation_fails();
-	ft_strlcpy(new_string, token, before_var_len);
-	ft_strcat(new_string, value);
-	ft_strcat(new_string, var_pos + ft_strlen(var));
-	return (new_string);
-}
+// 	var_pos = ft_strstr(token, var);
+// 	if (!var_pos)
+// 		return (ft_strdup(token));
+// 	before_var_len = var_pos - token;
+// 	total_len = ft_strlen(token) - ft_strlen(var) + ft_strlen(value);
+// 	new_string = ft_malloc(sizeof(char) * (total_len + 1));
+// 	if (!new_string)
+// 		allocation_fails();
+// 	ft_strlcpy(new_string, token, before_var_len);
+// 	ft_strcat(new_string, value);
+// 	ft_strcat(new_string, var_pos + ft_strlen(var));
+// 	return (new_string);
+// }
 
 // t_cmd	*parse_tokens(t_token	*tokens)
 // {
--- tools/tool4.c
+++ tools/tool4.c
@@ -6,48 +6,36 @@
 /*   By: abenzaho <abenzaho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 17:25:20 by ybenchel          #+#    #+#             */
-/*   Updated: 2025/04/08 17:41:11 by abenzaho         ###   ########.fr       */
+/*   Updated: 2025/04/08 17:49:39 by abenzaho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-t_token_type	token_type(char *str)
-{
-	if (ft_strcmp(str, "|") == 0)
-		return (PIPE);
-	else if (ft_strcmp(str, "<") == 0)
-		return (RED_IN);
-	else if (ft_strcmp(str, ">") == 0)
-		return (RED_OUT);
-	else if (ft_strcmp(str, ">>") == 0)
-		return (RED_APPEND);
-	else if (ft_strcmp(str, "<<") == 0)
-		return (HEREDOC);
-	else
-		return (WORD);
-}
+// t_token_type	token_type(char *str)
+// {
+// 	if (ft_strcmp(str, "|") == 0)
+// 		return (PIPE);
+// 	else if (ft_strcmp(str, "<") == 0)
+// 		return (RED_IN);
+// 	else if (ft_strcmp(str, ">") == 0)
+// 		return (RED_OUT);
+// 	else if (ft_strcmp(str, ">>") == 0)
+// 		return (RED_APPEND);
+// 	else if (ft_strcmp(str, "<<") == 0)
+// 		return (HEREDOC);
+// 	else
+// 		return (WORD);
+// }
 
-void	typeof_token(t_arg *arg)
-{
-	t_arg	*head;
+// void	typeof_token(t_arg *arg)
+// {
+// 	t_arg	*head;
 
-	head = arg;
-	while (head)
-	{
-		head->type = token_type(head->arg);
-		head = head->next;
-	}
-}
-
-t_arg	*ft_arglast(t_arg *lst)
-{
-	t_arg	*last;
-
-	if (!lst)
-		return (NULL);
-	last = lst;
-	while (last->next != NULL)
-		last = last->next;
-	return (last);
-}
+// 	head = arg;
+// 	while (head)
+// 	{
+// 		head->type = token_type(head->arg);
+// 		head = head->next;
+// 	}
+// }
