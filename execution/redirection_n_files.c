d235480c065f29c3669e274110c2ee851a4c2324
Abdellah Benzahour <abenzaho@e1r7p2.1337.ma>
2025-04-22 19:09:46 +0100

    ok

--- execution/redirection_n_files.c
+++ execution/redirection_n_files.c
@@ -6,7 +6,7 @@
 /*   By: abenzaho <abenzaho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/19 11:25:04 by ybenchel          #+#    #+#             */
-/*   Updated: 2025/04/22 17:57:59 by abenzaho         ###   ########.fr       */
+/*   Updated: 2025/04/22 19:09:42 by abenzaho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,45 +18,49 @@ void	in_n_out_backup(int *stdin_b, int *stdout_b)
 	*stdout_b = dup(STDOUT_FILENO);
 }
 
-int	check_redirection(t_token *tokens)
+int	check_redirection(t_file *files)
 {
-	int fd;
-	
-	if (ft_strcmp(tokens->cmds[0], ">") == 0)
-		fd = open(tokens->cmds[1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	else if (ft_strcmp(tokens->cmds[0], ">>") == 0)
-		fd = open(tokens->cmds[1], O_WRONLY | O_CREAT | O_APPEND, 0644);
-	else if (ft_strcmp(tokens->cmds[0], "<") == 0)
-		fd = open(tokens->cmds[1], O_RDONLY);
-	else
-		return (EXIT_FAILURE);
-	if (fd == -1)
-	{
-		perror(tokens->cmds[1]);
-		return (EXIT_FAILURE);
-	}
-	return (fd);	
+    int fd;
+    
+	fd = 0;
+    if (files->type == RED_OUT)
+        fd = open(files->file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    else if (files->type == RED_APPEND)
+        fd = open(files->file, O_WRONLY | O_CREAT | O_APPEND, 0644);
+    else if (files->type == RED_IN)
+        fd = open(files->file, O_RDONLY);
+    else if (files->type == HEREDOC)
+        fd = files->fd;
+    if (fd == -1)
+    {
+        perror(files->file);
+        return (EXIT_FAILURE);
+    }
+    if (files->type == RED_IN || files->type == HEREDOC)
+        dup_in(fd);
+    else 
+        dup_out(fd);
+        
+    return (EXIT_SUCCESS);	
 }
 
 void	check_herdoc(t_file *files)
 {
     char    *line;
-    int     fds[2];
     t_file  *current;
-    int     found_heredoc;
 
-    found_heredoc = 0;
-    if (pipe(fds) == -1)
-    {
-        perror("pipe error\n");
-        return ;
-    }
     current = files;
     while (current)
     {
         if (current->type == HEREDOC)
         {
-            found_heredoc = 1;
+            int fds[2];
+            if (pipe(fds) == -1)
+            {
+                perror("pipe error");
+                return;
+            }
+            current->fd = fds[0];
             line = readline("> ");
             while (line && ft_strcmp(line, current->file) != 0)
             {
@@ -67,19 +71,9 @@ void	check_herdoc(t_file *files)
             }
             if (line)
                 free(line);
+            close(fds[1]);
         }
         current = current->next;
-    }
-    
-    if (found_heredoc)
-    {
-        close(fds[1]);
-        dup_in(fds[0]);
-    }
-    else
-    {
-        close(fds[0]);
-        close(fds[1]);
     }
 }
 
