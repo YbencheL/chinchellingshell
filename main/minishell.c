1b057a670054d43d5a2d2b6b04eedf8d778659ad
Yassine Benchelh <ybenchel@e1r10p11.1337.ma>
2025-05-12 13:57:08 +0100

    final touches to the logic

--- main/minishell.c
+++ main/minishell.c
@@ -6,7 +6,7 @@
 /*   By: ybenchel <ybenchel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/15 14:13:52 by ybenchel          #+#    #+#             */
-/*   Updated: 2025/05/12 13:21:42 by ybenchel         ###   ########.fr       */
+/*   Updated: 2025/05/12 13:55:33 by ybenchel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,13 +83,29 @@ void	shell_loop(t_mp *pg)
 	clear_history();
 }
 
-int	main(int ac, char **av, char **env)
+void	pwd_shvl(t_mp *pg)
 {
-	t_mp	pg;
 	char cwd[PATH_MAX];
 	char *tmp;
 	int	count = 0;
 	char *shlvl;
+	
+	if (getcwd(cwd, sizeof(cwd)))
+	{
+		tmp = ft_strjoin("PWD=", cwd);
+		add_var(&pg->env, tmp);
+	}
+	//handling shell lvl
+	shlvl = getenv("SHLVL");
+	count = ft_atoi(shlvl);
+	tmp = ft_strjoin("SHLVL=", ft_itoa(count + 1));
+	add_var(&pg->env, tmp);
+	update_env(pg);
+}
+
+int	main(int ac, char **av, char **env)
+{
+	t_mp	pg;
 
 	pg.exit_status = 0;
 	pg.is_child = 0;
@@ -97,16 +113,7 @@ int	main(int ac, char **av, char **env)
 	check_args(ac, av);
 	pg.env = init_env(env);
 	pg.envp = env;
-	if (getcwd(cwd, sizeof(cwd)))
-	{
-		tmp = ft_strjoin("PWD=", cwd);
-		add_var(&pg.env, tmp);
-	}
-	//handling shell lvl
-	shlvl = getenv("SHLVL");
-	count = ft_atoi(shlvl);
-	tmp = ft_strjoin("SHLVL=", ft_itoa(count + 1));
-	add_var(&pg.env, tmp);
+	pwd_shvl(&pg);
 	// print_banner();
 	signal_setup();
 	in_n_out_backup(&pg);
